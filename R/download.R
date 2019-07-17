21670d56ebf1baee5f7a01b33857a7f23075c03d
Siqi Zhang <iqis.gnahz@gmail.com>
2019-07-17 11:40:31 -0400

    + pb_list_records()

--- R/download.R
+++ R/download.R
@@ -1,16 +1,29 @@
 .pb_index_url = "http://physionet.org/physiobank/database/"
 
-#' Get PhysioBank data catalog
+#' Get PhysioBank
 #'
 #' @return tibble
 #' @export
 #'
 #' @examples
 #' pb_catalog()
-pb_catalog <- function(){suppressWarnings({
+pb_get_db_catalog <- function(){suppressWarnings({
   httr::GET(file.path(.pb_index_url, "DBS")) %>%
     httr::content("text", encoding = "UTF-8") %>%
     stringr::str_replace_all("\t{3,}", "\t") %>%
     readr::read_tsv(col_names = FALSE) %>%
     magrittr::set_colnames(c("name", "description"))
 })}
+
+
+pb_list_records <- function(db_name){
+  httr::GET(file.path(.pb_index_url, db_name, "RECORDS")) %>%
+    httr::content("text") %>%
+    readr::read_lines()
+}
+
+pb_download_records <- function(db_name, target_path, flatten_dir = FALSE, overwrite = FALSE) {
+
+
+
+}
